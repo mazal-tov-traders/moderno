bfe2540a3af625ee0ecc3ccc2588f12e5ff20996
mazal-tov-traders <d90.german@gmail.com>
2022-08-02 19:03:08 +0300

    product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
-   
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+
+   var mixer = mixitup('.products__inner-box');
 
 });
