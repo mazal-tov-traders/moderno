88e665ba9935072d040eca9c89102a568cfdcd22
mazal-tov-traders <d90.german@gmail.com>
2022-09-14 14:18:40 +0300

    adaptiv heade and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   $('.product-slider__inner').slick({
     dots: true,
@@ -7,6 +7,44 @@ $(function(){
     slidesToScroll: 4
   });
 
-   var mixer = mixitup('.products__inner-box');
+  
+
+
+
+
+  $('.icon-list').on('click', function () {
+    $('.product__item').addClass('list');
+    $('.icon-list').addClass('active');
+    $('.icon-th-large').removeClass('avtive');
+  });
+  $('.icon-th-large').on('click', function () {
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('avtive');
+    $('.icon-list').removeClass('active');
+  });
+
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
+
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+
+
+
+  var mixer = mixitup('.products__inner-box');
 
 });
