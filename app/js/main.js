d2e28bfa9f978f45e318d182050aab64396670bc
mazal-tov-traders <d90.german@gmail.com>
2022-09-22 19:19:11 +0300

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ $(function () {
   });
 
   
-
+  
 
 
 
@@ -23,6 +23,15 @@ $(function () {
     $('.icon-list').removeClass('active');
   });
 
+  $('.product-one__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
 
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
@@ -33,13 +42,13 @@ $(function () {
   });
 
 
-  $(".js-range-slider").ionRangeSlider({
-    type: "double",
+  $('.js-range-slider').ionRangeSlider({
+    type: 'double',
     min: 0,
     max: 1000,
     from: 0,
     to: 600,
-    prefix: "$"
+    prefix: '$'
   });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('style', function(){
               //  Подключаем библиотеки
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',                 
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+      'node_modules/@fancyapps/ui/dist/fancybox.css'
         
     ])
         .pipe(concat('libs.min.css'))
@@ -38,8 +39,8 @@ gulp.task('script', function(){
               //  Подключаем библиотеки
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-        
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/@fancyapps/ui/dist/fancybox.umd.js'
         
     ])
         .pipe(concat('libs.min.js'))
