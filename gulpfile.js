64dc59313de9e236c9868895e8f601361cbce3e9
mazal-tov-traders <d90.german@gmail.com>
2022-10-01 20:04:52 +0300

    added adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,10 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',                 
         'node_modules/magnific-popup/dist/magnific-popup.css',
-      'node_modules/@fancyapps/ui/dist/fancybox.css'
+        'node_modules/@fancyapps/ui/dist/fancybox.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+        
         
     ])
         .pipe(concat('libs.min.css'))
@@ -40,7 +43,8 @@ gulp.task('script', function(){
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/@fancyapps/ui/dist/fancybox.umd.js'
+        'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
         
     ])
         .pipe(concat('libs.min.js'))
